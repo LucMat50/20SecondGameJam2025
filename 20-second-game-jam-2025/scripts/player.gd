7028d33331ec670f2d67c06cf587a86811045b44
LucMat50 <lmaturino5@gmail.com>
2025-11-23 14:16:50 -0800

    hehehe bullets

--- 20-second-game-jam-2025/scripts/player.gd
+++ 20-second-game-jam-2025/scripts/player.gd
@@ -1,8 +1,22 @@
 extends CharacterBody2D
 
+# EXPORT VARIABLES
 @export var speed = 400
 @export var rotation_speed = 100
 @export var acceleration = 5
+
+# ONREADY VARIABLES
+@onready var shooter = $Shooter
+
+# SCENES
+var bullet_scene = preload("res://scenes/bullet.tscn")
+
+# SIGNALS
+signal bullet_shot(bullet)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("shoot"):
+		shoot_bullet()
 
 func _physics_process(delta):
 	var input_vector = Vector2(0, Input.get_axis("move_up", "move_down"))
@@ -31,3 +45,9 @@ func _physics_process(delta):
 		global_position.x = screen_size.x + 200
 	elif global_position.x > screen_size.x + 200:
 		global_position.x = -200
+
+func shoot_bullet():
+	var heckYe = bullet_scene.instantiate()
+	heckYe.global_position = shooter.global_position
+	heckYe.rotation = rotation
+	emit_signal("bullet_shot", heckYe)
